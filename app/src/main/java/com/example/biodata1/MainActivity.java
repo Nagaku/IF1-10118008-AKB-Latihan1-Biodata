791c13a699ee9ac5f67b992dfdf68c11e08a5a85
Nagaku <mimelnagaku1@gmail.com>
2021-03-29 03:11:17 +0700

    2nd Commit

--- app/src/main/java/com/example/biodata1/MainActivity.java
+++ app/src/main/java/com/example/biodata1/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.biodata1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.core.view.ViewCompat;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Window;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        Typeface tf = ResourcesCompat.getFont(
+                this,
+                R.font.nunito);
+        TextView txt1 = findViewById(R.id.text1);
+        TextView txt2 = findViewById(R.id.text2);
+        TextView txt3 = findViewById(R.id.text3);
+        txt1.setTypeface(tf);
+        txt2.setTypeface(tf);
+        txt3.setTypeface(tf);
+
     }
 }
